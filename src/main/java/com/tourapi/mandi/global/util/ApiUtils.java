e6f6a68b48ce51e531543092f7b5d508bb30183d
ChaeyoungHa <hcy1722@korea.ac.kr>
2024-07-12 00:11:07 +0900

    #3 docs: API 응답 DTO swagger 적용

--- src/main/java/com/tourapi/mandi/global/util/ApiUtils.java
+++ src/main/java/com/tourapi/mandi/global/util/ApiUtils.java
@@ -1,6 +1,8 @@
 package com.tourapi.mandi.global.util;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class ApiUtils {
     public static <T> ApiResult<T> success(T response) {
         return new ApiResult<>(true, response, null);
@@ -10,6 +12,7 @@ public class ApiUtils {
         return new ApiResult<>(false, null, new ApiError(message, status, errorCode));
     }
 
+    @Schema(description = "정상 처리 시 응답 DTO")
     public record ApiResult<T>(
             boolean success,
             T response,
@@ -17,6 +20,7 @@ public class ApiUtils {
     ) {
     }
 
+    @Schema(description = "에러 발생 시 응답 DTO")
     public record ApiError(
             String message,
             int status,
