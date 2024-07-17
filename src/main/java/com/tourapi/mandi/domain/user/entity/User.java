a700f8e84d4e08a909fd57b430841c65139549aa
jhy0285 <jhy0285@gmail.com>
2024-07-17 20:25:57 +0900

    #7 feature : user엔티티 필드 추가

--- src/main/java/com/tourapi/mandi/domain/user/entity/User.java
+++ src/main/java/com/tourapi/mandi/domain/user/entity/User.java
@@ -4,6 +4,7 @@ import com.tourapi.mandi.domain.user.entity.constant.Provider;
 import com.tourapi.mandi.domain.user.entity.constant.Role;
 import com.tourapi.mandi.global.util.AuditingEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.Objects;
 
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "user_tb")
 public class User extends AuditingEntity {
@@ -26,7 +27,7 @@ public class User extends AuditingEntity {
     @Column(length = 100, nullable = false, unique = true, name = "email")
     private String email;
 
-    @Column(nullable = false, name = "password")
+    @Column(length = 255, nullable = false, name = "password")
     private String password;
 
     @Column(length = 30, nullable = false, name = "provider")
@@ -37,15 +38,29 @@ public class User extends AuditingEntity {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Column(length = 10, nullable = false, name = "nickname")
+    private String nickname;
+
+    @Column(length = 40, name = "description")
+    private String description;
+
+    @Column(length = 512, name = "img_url")
+    private String imgUrl;
+
     @Builder
-    public User(Long userId, String email, String password, Provider provider, Role role) {
+    public User(Long userId, String email, String password, Provider provider, Role role, String nickname, String description, String imgUrl) {
         this.userId = userId;
         this.email = email;
         this.password = password;
         this.provider = provider;
         this.role = role;
+        this.nickname = nickname;
+        this.description = description;
+        this.imgUrl=imgUrl;
+
     }
 
+    //userId가 같으면 같은 객체라고 판별되도록 equals, hashCode 오버라이드
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
