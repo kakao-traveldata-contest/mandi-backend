32603612420fa93f3cceb0d1829a1f90f5748528
jhy0285 <jhy0285@gmail.com>
2024-07-19 19:02:26 +0900

    #7 feature : signUpRequestDto 구현

---
+++ src/main/java/com/tourapi/mandi/domain/user/dto/SignupRequestDto.java
@@ -0,0 +1,18 @@
+package com.tourapi.mandi.domain.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Schema(description = "회원가입 요청 DTO")
+public record SignupRequestDto(
+        @Schema(description = "닉네임")
+        @Size(min = 2, max = 10, message = "닉네임은 2~10자 사이여야 합니다.")
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임은 영문, 숫자, 한글만 사용 가능합니다.")
+        String nickname,
+        @Schema(description = "한줄소개")
+        @Size(max = 40, message = "한줄소개는 공백 포함 40자를 넘을 수 없습니다.")
+        String description
+) {
+
+}
