a55020b7e047de9ba4f5d52e10cdafee6cd0be0b
jhy0285 <jhy0285@gmail.com>
2024-07-19 19:03:08 +0900

    #7 feature : LoginController에 회원가입 컨트롤러 추가

--- src/main/java/com/tourapi/mandi/domain/user/controller/LoginController.java
+++ src/main/java/com/tourapi/mandi/domain/user/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.tourapi.mandi.domain.user.controller;
 
 import com.tourapi.mandi.domain.user.dto.LoginResponseDto;
+import com.tourapi.mandi.domain.user.dto.SignupRequestDto;
 import com.tourapi.mandi.domain.user.dto.oauth.GoogleUserInfo;
 import com.tourapi.mandi.domain.user.service.GoogleService;
 import com.tourapi.mandi.domain.user.service.UserService;
@@ -8,14 +9,12 @@ import com.tourapi.mandi.global.util.ApiUtils;
 import com.tourapi.mandi.global.util.ApiUtils.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "로그인 API 목록")
 @RestController
@@ -36,6 +35,23 @@ public class LoginController {
     ) {
         GoogleUserInfo userInfo = googleService.getGoogleUserInfo(token);
         LoginResponseDto resultDto = userService.socialLogin(userInfo);
+        return ResponseEntity.ok(ApiUtils.success(resultDto));
+    }
+
+
+
+
+    @Operation(summary = "구글 소셜 회원가입 ")
+    @PostMapping("/google/signup")
+    public ResponseEntity<ApiResult<LoginResponseDto>> googleSignup(
+            @NotBlank
+            @RequestHeader(value = "Google") String token,
+            @RequestBody @Valid SignupRequestDto requestDto
+
+
+    ) {
+        GoogleUserInfo userInfo = googleService.getGoogleUserInfo(token);
+        LoginResponseDto resultDto = userService.socialSignup(userInfo,requestDto);
         return ResponseEntity.ok(ApiUtils.success(resultDto));
     }
 }
