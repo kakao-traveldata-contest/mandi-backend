61427994e4be9007e8258591e39489d22a6d9190
jhy0285 <jhy0285@gmail.com>
2024-08-25 15:06:23 +0900

    #14 블랙리스트 토큰 기능 완성

--- src/main/java/com/tourapi/mandi/global/redis/repository/BlackListTokenRepository.java
+++ src/main/java/com/tourapi/mandi/global/redis/repository/BlackListTokenRepository.java
@@ -1,2 +1,10 @@
-package com.tourapi.mandi.global.redis.repository;public interface BlackListTokenRepository {
+package com.tourapi.mandi.global.redis.repository;
+
+import com.tourapi.mandi.global.redis.entity.BlackListToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface BlackListTokenRepository  extends CrudRepository<BlackListToken, String> {
+    Optional<BlackListToken> findById(String accessToken);
 }
--- src/main/java/com/tourapi/mandi/global/redis/service/BlackListTokenService.java
+++ src/main/java/com/tourapi/mandi/global/redis/service/BlackListTokenService.java
@@ -1,2 +1,32 @@
-package com.tourapi.mandi.global.redis.service;public class BlackListTokenService {
+package com.tourapi.mandi.global.redis.service;
+import com.tourapi.mandi.global.exception.Exception403;
+import com.tourapi.mandi.global.redis.RedisExceptionStatus;
+import com.tourapi.mandi.global.redis.entity.BlackListToken;
+import com.tourapi.mandi.global.redis.repository.BlackListTokenRepository;
+import com.tourapi.mandi.global.security.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BlackListTokenService {
+
+        private final BlackListTokenRepository blackListTokenRepository;
+
+        public void save(String accessToken) {
+        Long remainTime = JwtProvider.getRemainExpiration(accessToken);
+        BlackListToken blackListToken = BlackListToken.builder()
+                .accessToken(accessToken)
+                .expiration(remainTime)
+                .build();
+
+        blackListTokenRepository.save(blackListToken);
+    }
+
+        public void validAccessToken(String accessToken) {
+        if (blackListTokenRepository.existsById(accessToken)) {
+            throw new Exception403(RedisExceptionStatus.BLACKLIST_TOKEN);
+        }
+    }
+
 }
