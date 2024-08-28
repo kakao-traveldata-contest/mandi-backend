44cd21db9dc62941b84f40c909a853134f1a1ef1
LEE-sh1673 <lsh901673@gmail.com>
2024-08-28 23:32:34 +0900

    #18 docs: API 문서 JWT 인증 버튼 설정

--- src/main/java/com/tourapi/mandi/global/config/OpenAPIConfig.java
+++ src/main/java/com/tourapi/mandi/global/config/OpenAPIConfig.java
@@ -1,18 +1,42 @@
 package com.tourapi.mandi.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class OpenAPIConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("만디 API 문서")
-                        .version("1.0.0")
-                );
+                .info(apiInfo())
+                .components(bearerTokenComponents())
+                .addSecurityItem(bearerTokenRequirement());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("만디 API 문서")
+                .version("1.0.0");
+    }
+
+    private Components bearerTokenComponents() {
+        return new Components().addSecuritySchemes("Bearer Token", jwtSecurityScheme());
+    }
+
+    private SecurityScheme jwtSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+    }
+
+    private SecurityRequirement bearerTokenRequirement() {
+        return new SecurityRequirement().addList("Bearer Token");
     }
 }
