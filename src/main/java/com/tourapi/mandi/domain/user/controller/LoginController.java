fe161f11b7ef81239a168098d82cfb53e4043e24
LEE-sh1673 <lsh901673@gmail.com>
2024-08-30 00:19:39 +0900

    #18 docs: 로그인 API 목록 설명 추가
    - 각 API에 대한 응답 코드 및 설명 추가

--- src/main/java/com/tourapi/mandi/domain/user/controller/LoginController.java
+++ src/main/java/com/tourapi/mandi/domain/user/controller/LoginController.java
@@ -8,6 +8,10 @@ import com.tourapi.mandi.domain.user.service.UserService;
 import com.tourapi.mandi.global.util.ApiUtils;
 import com.tourapi.mandi.global.util.ApiUtils.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -27,11 +31,16 @@ public class LoginController {
     private final UserService userService;
 
     @Operation(summary = "구글 소셜 로그인")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 access token 입력시"),
+            @ApiResponse(responseCode = "500", description = "Google API 연동 중 문제 발생시"),
+    })
     @PostMapping("/google/login")
     public ResponseEntity<ApiResult<LoginResponseDto>> googleLogin(
             @NotBlank
             @RequestHeader(value = "Google")
-            String token
+            @Parameter(description = "Google access token", required = true) String token
     ) {
         GoogleUserInfo userInfo = googleService.getGoogleUserInfo(token);
         LoginResponseDto resultDto = userService.socialLogin(userInfo);
@@ -39,10 +48,16 @@ public class LoginController {
     }
 
     @Operation(summary = "구글 소셜 회원가입 ")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "1. 유효하지 않은 access token 입력시\n2. 유효하지 않은 닉네임/한줄소개 입력시"),
+            @ApiResponse(responseCode = "500", description = "Google API 연동 중 문제 발생시"),
+    })
     @PostMapping("/google/signup")
     public ResponseEntity<ApiResult<LoginResponseDto>> googleSignup(
             @NotBlank
-            @RequestHeader(value = "Google") String token,
+            @RequestHeader(value = "Google")
+            @Parameter(description = "Google access token", required = true) String token,
             @RequestBody @Valid SignupRequestDto requestDto
     ) {
         GoogleUserInfo userInfo = googleService.getGoogleUserInfo(token);
