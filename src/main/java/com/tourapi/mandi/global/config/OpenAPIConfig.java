5806eae41179046897ad6c14550a934c63b9651b
LEE-sh1673 <lsh901673@gmail.com>
2024-08-30 17:39:53 +0900

    #18 docs: API 문서 그룹화 설정

--- src/main/java/com/tourapi/mandi/global/config/OpenAPIConfig.java
+++ src/main/java/com/tourapi/mandi/global/config/OpenAPIConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -40,5 +41,29 @@ public class OpenAPIConfig {
 
     private SecurityRequirement bearerTokenRequirement() {
         return new SecurityRequirement().addList("Bearer Token");
+    }
+
+    @Bean
+    public GroupedOpenApi authOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("로그인 API")
+                .pathsToMatch("/auth/**", "/dev/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi profileOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("프로필 API")
+                .pathsToMatch("/profile/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi badgeOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("뱃지 API")
+                .pathsToMatch("/badges/**")
+                .build();
     }
 }
