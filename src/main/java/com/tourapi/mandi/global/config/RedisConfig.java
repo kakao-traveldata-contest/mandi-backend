72184b504fdcef4fb6206e0142eea0fc46b04b86
LEE-sh1673 <lsh901673@gmail.com>
2024-09-23 18:09:13 +0900

    48 fix: Bean override error 수정

--- src/main/java/com/tourapi/mandi/global/config/RedisConfig.java
+++ src/main/java/com/tourapi/mandi/global/config/RedisConfig.java
@@ -1,13 +1,9 @@
 package com.tourapi.mandi.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
 
@@ -15,26 +11,12 @@ import java.time.Duration;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
     @Bean
-    public ReactiveRedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(host);
-        config.setPort(port);
-        return new LettuceConnectionFactory(config, lettuceClientConfigurationBuilder());
-    }
-
-    @Bean
-    public LettuceClientConfiguration lettuceClientConfigurationBuilder() {
-        return LettuceClientConfiguration.builder()
-                .useSsl()
-                .and()
-                .commandTimeout(Duration.ofSeconds(30))
-                .build();
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigCustomize() {
+        return builder -> {
+            builder.useSsl();
+            builder.commandTimeout(Duration.ofSeconds(10));
+            builder.shutdownTimeout(Duration.ZERO);
+        };
     }
 }
