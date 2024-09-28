cdea66cc59abe35119d2f8a1151238d5bab4c7ec
LEE-sh1673 <lsh901673@gmail.com>
2024-09-28 18:18:04 +0900

    #42 chore: 배지 데이터 수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,20 +1,20 @@
 INSERT INTO badge_tb(img_url, name, requirements, created_at, last_modified_at)
-VALUES ('test_img01', 'badge01', 'conditions01', now(), now());
+VALUES ('https://mandi-courses.s3.ap-northeast-2.amazonaws.com/badge01.png', 'Mandi Starter', 'Join the Mandi service', now(), now());
 
 INSERT INTO badge_tb(img_url, name, requirements, created_at, last_modified_at)
-VALUES ('test_img02', 'badge02', 'conditions02', now(), now());
+VALUES ('https://mandi-courses.s3.ap-northeast-2.amazonaws.com/badge02.png', 'Course Collector', '3 or more course bookmarks', now(), now());
 
 INSERT INTO badge_tb(img_url, name, requirements, created_at, last_modified_at)
-VALUES ('test_img03', 'badge03', 'conditions03', now(), now());
+VALUES ('https://mandi-courses.s3.ap-northeast-2.amazonaws.com/badge03.png', 'Joy of Sharing', 'First Review Written', now(), now());
 
 INSERT INTO badge_tb(img_url, name, requirements, created_at, last_modified_at)
-VALUES ('test_img04', 'badge04', 'conditions04', now(), now());
+VALUES ('https://mandi-courses.s3.ap-northeast-2.amazonaws.com/badge04.png', 'Beginning of Completion', 'Completing the first course', now(), now());
 
 INSERT INTO badge_tb(img_url, name, requirements, created_at, last_modified_at)
-VALUES ('test_img05', 'badge05', 'conditions05', now(), now());
+VALUES ('https://mandi-courses.s3.ap-northeast-2.amazonaws.com/badge05.png', 'Walked 10,000 Steps', 'Total distance achieved at least 8 km', now(), now());
 
 INSERT INTO badge_tb(img_url, name, requirements, created_at, last_modified_at)
-VALUES ('test_img06', 'badge06', 'conditions06', now(), now());
+VALUES ('https://mandi-courses.s3.ap-northeast-2.amazonaws.com/badge06.png', 'Mandiholic', 'More than 10 visits to Mandi', now(), now());
 
 INSERT INTO user_tb(description, email, img_url, nickname, password, provider, role, created_at, last_modified_at)
 VALUES
